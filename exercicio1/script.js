9ef6c94d852e1c3845e4789331e64ba42135f87f
christhian brandão <christhianbrandao69@gmail.com>
2022-08-09 17:59:24 -0300

    terminei

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,14 @@
-const num = //seu numero aqui
+const num = Number(prompt("digite o numero aqui!!!"))
+
+//atividade 1
+if(num % 2 ===0){
+    if(num % 3 ===0){
+        console.log("seu numero é divisivel por 2 e por 3")
+    }
+}
+
+//atividade 2
+if(num % 2 ===0 || num % 3 ===0){
+    console.log("seu numero é divisivel por 2 ou por 3")
+}
 
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,5 +1,8 @@
 //código a ser reescrito
 
+let nome = 'José'
+let idade = Number(prompt("digite sua idade"))
+/*
 if(nome === "José"){
 	console.log("Oi, Zé!");
 } else {
@@ -10,4 +13,8 @@ if(idade >= 18){
 	console.log("pode tirar carteira de motorista!");
 } else {
 	console.log("Ainda não pode tirar carteira de motorista");
-}
+}*/
+
+console.log(nome === "josé"? "Olá, José": "Ola,nome")
+
+console.log(idade>=18?"Pode tirar carteira de motorista":"Ainda nãopode tirar carteira de motorista")
