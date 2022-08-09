2baab537dfd9d6929db42a706b4b26d950a73317
Lucas Bonfanti <lucasbonfanti97@gmail.com>
2022-08-09 19:29:49 -0300

    finalização dos exercícios

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,16 +1,20 @@
 const num = +prompt('Digite aqui um número')
 
 //1
-// if(num%2===0){
-//     console.log('O número é divisível por 2')
-// }if(num%3===0){
-//     console.log('Divisível por 3')
+//  if(num%2===0){
+//      console.log('O número é divisível por 2')
+//  }if(num%3===0){
+//      console.log('Divisível por 3')
+// }else{
+//     console.log('o número não é divisível por 2 ou 3')
 // }
 
 //2
 if(num%2===0 || num%3===0){
     console.log('Divisível por 2 ou por 3')
-}
+}else{
+    console.log('o número não é divisível por 2 ou 3')
+    }
     
     
 
