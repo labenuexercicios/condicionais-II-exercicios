bbadf348513c41813e7c81fb8a3caecd02256c6c
pedrohochmann <pedrohochmann@gmail.com>
2022-08-09 19:37:08 -0300

    Barbosa-A-Pedro-Hochmann

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,8 @@
 //código a ser reescrito
+nome = prompt("Digite aqui seu nome:")
+nome === "José" ? console.log("Oi, Zé!"):
+console.log("Olá, "+nome);
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+idade = prompt("Digite aqui sua idade:")
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+idade >= 18 ? console.log("pode tirar carteira de motorista!"):console.log("Ainda não pode tirar carteira de motorista")
