901d0d6d30e40286363424a71dcb5f54d35d4674
nosdliw <108556065+nosdliw@users.noreply.github.com>
2022-08-09 19:56:14 -0300

    atividade 3, ok!

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,18 @@
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+
+const nome = ("José")
+nome === "José" ? alert("Oi, Zé!") : Alert("olá", + nome)
+const idade = Number(prompt("Qual a sua idade?"))
+idade >= 18 ? alert("Pode tirar a carteira de motorista!") : Alert("Ainda não pode tirar a carteira de motorista, Zé")
