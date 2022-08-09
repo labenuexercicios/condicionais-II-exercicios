8929f9e593f0b529a4974a8167a9c75878da4e88
Jonathan-Moreira <108627997+Jonathan-Moreira@users.noreply.github.com>
2022-08-09 18:54:03 -0300

    exercicio-condicionais-a013- feito

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,21 @@
-const num = //seu numero aqui
 
+const Number = +prompt("digite um numero?")
+
+if (Number % 2 === 0) {
+    console.log("o numero é divisivel por dois")
+    if (Number % 3 === 0) {
+        console.log("o numero é divisivel por tres")
+    } else {
+        console.log("o numero não é divisivel tres")
+    }
+} else {
+    console.log("o numero não é divisivel por dois")
+}
+
+
+
+if (Number % 2 === 0 || Number % 3 === 0) {
+    console.log("o numero é divisivel")
+} else {
+    console.log("o numero não é divisivel")
+}
