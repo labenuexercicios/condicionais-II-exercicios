8534f5a36005098aaf51c039a2ae5fa8ece30e13
nosdliw <108556065+nosdliw@users.noreply.github.com>
2022-08-09 19:06:29 -0300

    exercício 1, OK!

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,13 @@
-const num = //seu numero aqui
+const num = Number(prompt("digite um número e direi se ele é divisível por dois ou por três"))//seu numero aqui
+
+if(num % 2 === 0){
+    alert("o número é divisível por 2")
+    if(num % 3 === 0){
+        alert("O número é divisível por 3")
+        if(num % 2 === 0 && num % 3 === 0){
+            alert("O número é divisível por ambos os números!")
+        }
+    }
+}
+
 
