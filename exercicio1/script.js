c6377134e5c53cb004ddd1e5f0c7e17172b7e4e8
Dhieuri <dhieuri@gmail.com>
2022-08-08 23:05:12 -0300

    Exercicios a013-condicionais

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,17 @@
-const num = //seu numero aqui
+const num =Number(prompt('Insira um numero:'))
 
+// if(num % 2 === 0){
+//     if(num % 3 === 0){
+//       console.log('Esse numero é divisivel por 2 e 3.')
+//     }else{
+//         console.log('Esse número não é divisível por 2 e 3.')
+//     }
+// }else{
+//     console.log('Esse número não é divisível por 2 e 3.')
+// }
+// operador logico
+if(num % 2 === 0 && num % 3 === 0){
+    console.log("Esse numero é divisivel por 2 e 3.")
+}else{
+    console.log('Esse numero não é divisivel por 2 e 3.')
+}
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,20 @@
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+
+const nome =prompt("Qual o seu nome?")
+const idade=Number(prompt('Qual sua idade?'))
+
+console.log(nome==="José"? 'Oi, Zé!' : 'Olá,'+ nome);
+
+console.log(idade>=18 ? "Você já pode tirar a carteira de motorista!":'Você ainda não pode tirar a carteira de motorista.')
