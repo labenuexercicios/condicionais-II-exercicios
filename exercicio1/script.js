624b42c1bdce44c446dcc53c22417f9e25c951de
JulianDiel <drdiel@hotmail.com>
2022-08-09 15:33:36 -0300

    Exercicios

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,19 @@
-const num = //seu numero aqui
+const num = Number(prompt(`Digite um numero`))
 
+if(num %2 == 0 && num %3 == 0){
+    console.log(`O numero é divisivel por 2 e por 3.`, num);
+}else{
+    
+    if (num % 2 == 0 || num % 3 == 0){
+        if(num %2 == 0){
+            console.log(`O numero é divisivel por 2.`,
+            num);      
+        }else{
+           console.log(`O numero é divisivel por 3.`,
+            num);
+        }
+}else{
+     console.log(`O numero  não é divisivel por 2 e nem 3.`,
+     num);
+     } 
+}
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,37 @@
 let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
 
-if(nacionalidade === "brasileira"){
+// if(nacionalidade === "brasileira"){
+//     console.log("a pessoa é do Brasil!");
+// } else if(nacionalidade === "argentina"){
+//     console.log("a pessoa é da Argentina!");
+// } else if(nacionalidade === "uruguaia"){
+//     console.log("a pessoa é do Uruguai!");
+// } else if(nacionalidade === "chilena"){
+//     console.log("a pessoa é do Chile!");
+// } else if(nacionalidade === "colombiana"){
+//     console.log("a pessoa é da Colômbia!");
+// } else{
+//     console.log("nacionalidade não encontrada")
+// }
+
+switch (nacionalidade) {
+    case "brasileira":
     console.log("a pessoa é do Brasil!");
-} else if(nacionalidade === "argentina"){
+        break;
+        case "argentina":
     console.log("a pessoa é da Argentina!");
-} else if(nacionalidade === "uruguaia"){
+        break;
+        case "uruguaia":
     console.log("a pessoa é do Uruguai!");
-} else if(nacionalidade === "chilena"){
+        break;
+        case "chilena":
     console.log("a pessoa é do Chile!");
-} else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!");
-} else{
-    console.log("nacionalidade não encontrada")
+        break;
+        case "colombia":
+    console.log("a pessoa é traficante de Cocaína, Ops...  Colombia xD!");
+        break;
+
+    default:
+        console.log("Nacionalidade nao Registrada!");
+        break;
 }
