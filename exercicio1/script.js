e64179bfe363716f0f3667b395eba9d6baf90bd1
DavidAlves28 <dav.alvesc28@gmail.com>
2022-08-09 20:39:17 -0300

    barbosa-david-alvescosta

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,17 @@
-const num = //seu numero aqui
+const num = Number(prompt("COLOQUE ALGUM NÚMERO AQUI"))
+
+if (num) {
+    if (num % 3 === 0) { console.log("Divisível por 3"); }
+    if (num % 3 !== 0) { console.log("Não é divisível por 3"); }
+    if (num % 2 === 0) { console.log("Divisível por 2"); }
+    if (num % 2 !== 0) { console.log("Não é divisível por 2"); }
+} else { console.log('não encontrado, tente um número'); }
+
+
+// if (num%2=== 0 || num%3=== 0) {
+//     console.log('Este número é divisível por 3 OU 2 .');
+//     if (num%3 === 0) console.log("divisível por 3")
+//     if (num%2 === 0) console.log("divisível por 2")
+// } else { console.log('Tente outro número.'); }
+
 
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,17 @@
 //código a ser reescrito
+const nome = "José";
+const idade = 12;
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
-
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+// nome === "José" && idade >=18 ? console.log("Oi,Zé! Voce pode tirar carteira de motorista!" ) : console.log("Oi,Zé","Ainda não pode tirar carteira de motorista");
+//  nome !=="José" && idade>=18 ? console.log("Oi olá,"+nome,"! Voce pode tirar carteira de motorista!" ) : console.log("Olá, "+nome,"Ainda não pode tirar carteira de motorista");
+ (nome === "José" ||nome !=="José")&& idade >=18 ? console.log("Oi,"+nome,"! Voce pode tirar carteira de motorista!" ) : console.log("Oi,"+nome,"Ainda não pode tirar carteira de motorista");
