c5ba281d9ef9b5770d4247b3fc521c3f7c37d8b2
Leo Carrati <carrati3@gmail.com>
2022-08-09 17:46:38 -0300

    terminei

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,20 @@
-//código a ser reescrito
+// //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+
+const nome = prompt("qual seu nome?")
+const idade = prompt("qual sua idade?")
+
+console.log(nome === "José" ? "Oi, Zé!" : "Olá, "+nome);
+
+console.log(idade >=18 ? "pode tirar carteira de motorista!" : "Ainda não pode tirar carteira de motorista");
