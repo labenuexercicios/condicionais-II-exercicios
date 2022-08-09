ac960acc19875982ee2eaf6f8da1ff74f140fdcd
Eduardo Fernandes <eduardo.eduesf@gmail.com>
2022-08-09 18:57:03 -0300

    Entrega dos 3 exercícios de Condicionais 2

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,29 @@
-const num = //seu numero aqui
+// Resolução Exercício 1
 
+// a)
+
+const num = Number(prompt("Informe um número que seja divisível por 2 ou por 3"))
+
+if (num % 2 === 0){
+    if(num % 3 === 0){
+        console.log ("O número informado é divisível por 2 e por 3")
+    } else {
+        console.log ("O número informado é divisível por 2, mas NÃO é divisível por 3")
+    }
+} else if(num % 3 === 0) {
+    console.log ("O número informado é divisível por 3, mas NÃO é divisível por 2")
+} else {
+    console.log ("O número informado NÃO é divisível por 2")
+}
+
+
+
+// b)
+
+const num1 = Number(prompt("Informe um número que seja divisível por 2 ou por 3"))
+
+if(num1 % 2 === 0 || num1 % 3 === 0){
+    console.log ("O número informado é divisível por 2 ou por 3")
+}else {
+    console.log ("O número informado NÃO é divisível por 2 ou por 3")
+}
