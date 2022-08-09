1fdfb4229e4424245afa317a0a1a93a643008861
LorenzoBaridotti <lorenzo.baridotti@hotmail.com>
2022-08-09 20:11:51 -0300

    Exercícios a013

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,19 @@
-const num = //seu numero aqui
+const numero = +prompt('Digite um numero divisivel por 2 ou por 3')
 
+//com ifs aninhados
+
+if(numero % 2 === 0){
+    console.log('O numero é divisivel por 2')
+if(numero % 3 === 0){
+    console.log('O numero é divisivel por 3')}
+}else{
+    console.log('O numero não é divisivel por 2 ou por 3')
+}
+
+//com operadores logicos
+
+if(numero % 2 === 0 || numero % 3 === 0){
+    console.log('O numero é divisivel por 2 ou por 3')}
+else{
+    console.log('O numero não é divisivel por 2 ou por 3')
+}
