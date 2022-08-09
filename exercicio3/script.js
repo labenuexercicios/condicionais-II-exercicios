d0a583fd0e28872c2f289793d7031817cc9871ca
Lucas Bonfanti <lucasbonfanti97@gmail.com>
2022-08-08 21:37:47 -0300

    Exercicio 3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,16 @@
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+const nome = prompt('Qual seu nome?')
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+console.log(nome === 'José'? 'Oi, Zé!': `Olá, ${nome}`)
+
+
+
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+
+const idade = +prompt('Qual sua idade?')
+console.log(idade>=18? 'Pode tirar carta de motorista!':'Ainda não pode tirar carta de motorista')
