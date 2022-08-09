71777107e5eaab273047e01a36c8663f0ed1457e
Rebecca Ferreira <beccixzin@gmail.com>
2022-08-08 22:36:24 -0300

    finalizado

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,38 @@
 let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
+const nacionalidadeSliced = nacionalidade.slice(0, -1); // mais simples
+// const nacionalidadeSliced = nacionalidade.slice(0, nacionalidade.length - 1)
 
-if(nacionalidade === "brasileira"){
-    console.log("a pessoa é do Brasil!");
-} else if(nacionalidade === "argentina"){
-    console.log("a pessoa é da Argentina!");
-} else if(nacionalidade === "uruguaia"){
-    console.log("a pessoa é do Uruguai!");
-} else if(nacionalidade === "chilena"){
-    console.log("a pessoa é do Chile!");
-} else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!");
-} else{
-    console.log("nacionalidade não encontrada")
+
+// if(nacionalidade === "brasileira"){
+//     console.log("a pessoa é do Brasil!");
+// } else if(nacionalidade === "argentina"){
+//     console.log("a pessoa é da Argentina!");
+// } else if(nacionalidade === "uruguaia"){
+//     console.log("a pessoa é do Uruguai!");
+// } else if(nacionalidade === "chilena"){
+//     console.log("a pessoa é do Chile!");
+// } else if(nacionalidade === "colombiana"){
+//     console.log("a pessoa é da Colômbia!");
+// } else{
+//     console.log("nacionalidade não encontrada")
+// }
+
+switch(nacionalidadeSliced) {
+    case 'brasileir':
+        console.log("a pessoa é do Brasil!");
+        break;
+    case 'argentin':
+        console.log("a pessoa é da Argentina!");
+        break;
+    case 'uruguai':
+        console.log("a pessoa é do Uruguai!");
+        break;
+    case 'chilen':
+        console.log("a pessoa é do Chile!");
+        break;
+    case 'colombian':
+        console.log("a pessoa é da Colômbia!");
+        break;
+    default:
+        console.log("nacionalidade não encontrada");
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,21 @@
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+
+const nome = prompt("Insira um nome");
+
+console.log(nome === "José" ? `Oi, Zé!` : `Olá, ${nome}`);
+
+const idade = +prompt("Insira um número");
+
+console.log(idade >= 18 ? "Pode tirar carteira de motorista!" : "Ainda não pode tirar carteira de motorista");
