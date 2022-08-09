686a5e6936dc4a61df7630e0037e05593f72f7af
g0nc41ves <daniel4671@hotmail.com>
2022-08-09 19:40:13 -0300

    Respostas

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,40 @@
-const num = //seu numero aqui
+// # Exercício 1
 
+// Crie um código que receba um número por **prompt** e verifique se um número é divisível por 2 **ou** por 3
+    
+// Faça isso:
+    
+// 1. Utilizando ifs aninhados
+// ```jsx
+//     if(expressao){
+//         if(expressao){
+
+//         }
+//     }
+// ```
+// 2. Utilizando um operador lógico para unir duas operações relacionais
+// ```jsx
+//     if(expressao && expressao){ //&& para E
+//     //expressao || expressao para OU     
+    
+//     }
+// ```
+
+
+
+// const num = Number(prompt("Insira um número que seja divisível por 2 e por 3:"))
+
+// if(num %2 === 0){
+//     if(num %3 === 0){
+//         alert("Seu número é divisível por 2 e por 3")
+//     }
+// }else{
+//     alert("Tente outro número")
+// }
+
+const num = Number(prompt("Insira um número que seja divisível por 2 ou por 3:"))
+if(num %2 === 0 || num %3 === 0){
+    alert("Seu número é divisível por 2 ou por 3")
+}else{
+    alert("Tente outro número")
+}
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,38 @@
+
+// # Exercício 3
+
+// Reescreva os códigos do arquivo script.js utilizando o if ternário.
+
+// **Relembrando o if ternário**
+// ```jsx
+// condição ? expr1 : expr2
+// ```
+
+// Onde :
+
+// `condição` é uma expressão que é avaliada como `true` ou `false`.
+
+// `expr1`, `expr2` são xpressões com valores de qualquer tipo.
+
+
+
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
+
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+
+const nome = prompt("Insira seu nome aqui:")
+nome === "José" ? alert("Oi, Zé!") : alert("Olá, " +nome);
+
+let idade = Number(prompt("Insira a sua idade aqui:"))
+idade >= 18 ? alert("pode tirar carteira de motorista!") : alert("Ainda não pode tirar carteira de motorista");
