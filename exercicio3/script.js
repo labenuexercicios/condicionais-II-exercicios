db77fbb3b2c4f3c4a214212a4769d2973ca2f1eb
Thaila Marani <thaila_marani@hotmail.com>
2022-08-09 15:08:57 -0300

    Realização dos exercícios 1, 2 e 3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,21 @@
 //código a ser reescrito
+const nome = prompt("Digite aqui seu nome")
 
-if(nome === "José"){
+/*if(nome === "José"){
 	console.log("Oi, Zé!");
 } else {
 	console.log("Olá, "+nome);
-}
+}*/
 
-if(idade >= 18){
+nome === "José" ? console.log("Oi, Zé!") : console.log("Olá, "+nome);
+
+
+const idade = Number(prompt("Digite sua idade"));
+
+/*if(idade >= 18){
 	console.log("pode tirar carteira de motorista!");
 } else {
 	console.log("Ainda não pode tirar carteira de motorista");
-}
+}*/
+
+idade >=18 ? console.log("Pode tirar carteira de motorista!") : console.log("Ainda não pode tirar carteira de motorista!");
