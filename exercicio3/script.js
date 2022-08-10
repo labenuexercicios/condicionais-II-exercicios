f63cdcfa49f40cc43f71b337ca212c4f88ffec1b
=Emerson78costad <emersoncostadantas1995@gmail.com>
2022-08-09 23:21:25 -0400

    exercicio-condicionais-II

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,18 @@
 //código a ser reescrito
-
+let nome = prompt("digite seu nome ")
 if(nome === "José"){
 	console.log("Oi, Zé!");
 } else {
 	console.log("Olá, "+nome);
 }
-
+/*
 if(idade >= 18){
 	console.log("pode tirar carteira de motorista!");
 } else {
 	console.log("Ainda não pode tirar carteira de motorista");
 }
+*/
+let idade = Number(prompt("qual é a sua idade "))
+
+idade >=18 ? console.log("voce pode tirar carteira de motorista") 
+: console.log("ainda nao pode tirar carteira de motorista")
