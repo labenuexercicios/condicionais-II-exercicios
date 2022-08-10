67282675ff02b6763bf50b929f767d0459a7d3ca
Filipe Gomes <seu@email.com>
2022-08-09 21:26:25 -0300

    Barbosa-A-Filipe-Gomes-a013

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,5 +1,5 @@
 let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
-
+/*
 if(nacionalidade === "brasileira"){
     console.log("a pessoa é do Brasil!");
 } else if(nacionalidade === "argentina"){
@@ -12,4 +12,24 @@ if(nacionalidade === "brasileira"){
     console.log("a pessoa é da Colômbia!");
 } else{
     console.log("nacionalidade não encontrada")
-}
+}*/
+
+switch(nacionalidade){
+    case "brasileira":
+        console.log('a pessoa é do Brasil!')
+        break;
+    case "argentina":  
+        console.log('a pessoa é da Argentina!')
+        break;
+    case "Uruguaia":
+        console.log('a pessoa é do Uruguaia!')
+        break;
+    case "Chilena":
+        console.log('a pessoa é do Chile!')
+        break;
+    case "Colombiana":
+        console.log('a pessoa é da Colômbia!')
+        break;
+    default:
+        console.log('nacionalidade não encontrada')
+    }
