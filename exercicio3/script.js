bac0b3b3435bd91416eae58e9669279fc341c1a0
Guilherme <vigilato.sp@hotmail.com>
2022-08-21 19:11:53 -0300

    Atualização da aula a013-condicionais2

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,5 @@
-//código a ser reescrito
+const nome = prompt("Qual é seu nome?")
+const idade = Number(prompt("Qual sua idade?"))
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
-
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+console.log(nome === "José"? "Oi Zé" : "Olá " + nome);
+console.log(idade >= 18? "Pode tirar carteira de motorista!" : "Ainda não pode tirar carteira de motorista");
