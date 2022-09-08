5da54408346d07d8d52ef996feda8dd935ae7537
milenarismo <milenasimoeslabenu@gmail.com>
2022-09-08 16:14:58 -0300

    Exercício

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,17 @@
-const num = //seu numero aqui
+const num = Number(prompt("Insira um número"))
 
+if(num % 2 === 0){
+    console.log("Esse número é divisivel por 2")
+   } else if(num % 3 === 0){
+    console.log("Esse número é divisivel por 3")
+    //    } else{
+    // console.log("esse número não é divisivel nem por 2 nem por 3")
+}
+
+
+
+if(num % 2 === 0 || num % 3 === 0){
+    console.log("Esse número é divisivel por 2 ou por 3")
+} else{
+    console.log("Esse número não é divisível nem por 2 nem por 3")
+}
