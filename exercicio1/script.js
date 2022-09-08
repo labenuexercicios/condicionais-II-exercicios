c0c6d892252b32953131db28b8773658c19a0728
everandrade <eversonandrade79@gmail.com>
2022-09-08 19:01:01 -0300

    Exercícios resolvidos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,26 @@
-const num = //seu numero aqui
+const num = +prompt("Qual é seu número?")
 
+if (num % 2 === 0) {
+    if (num % 3 === 0) {
+        console.log("O número é divisivel por 2 ou 3!")
+    }
+    else {
+        console.log("O número é divisivel por 2 ou 3!")
+    }
+}
+else {
+    if (num % 3 === 0) {
+        console.log("O número é divisivel por 2 ou 3!!")
+    }
+    else {
+        console.log("Não é divisivel por 2 ou por 3!")
+    }
+}
+
+//Com operador lógico
+if ((num % 2 === 0) || (num % 3 === 0)) {
+    console.log("O número é divisivel por 2 ou 3!!")
+}
+else {
+    console.log("Não é divisivel por 2 ou por 3!")
+}
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,7 @@
-//código a ser reescrito
+let nome = prompt("Qual é seu nome?").toLowerCase()
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+console.log(nome === "josé" ? "Oi, Zé" : `Olá, ${nome}!`)
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+let idade = prompt("Qual é sua idade?")
+
+console.log(idade >= 18 ? "Oi, pode tirar carteira de motorista!" : "Olá, ainda não pode tirar carteira de motorista!")
