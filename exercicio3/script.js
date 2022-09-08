57c5b57ff82cf1789361637aef73c99575fd684f
Jessica <jessica.thamiresnunes@gmail.com>
2022-09-08 19:35:05 -0300

    Atividade realizada com sucesso

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,12 @@
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+const nome= prompt("Diga seu nome?")
+nome==="José" ? console.log("Oi,  Zé!") : console.log("Olá, "+ nome) 
+
+
+const idade= prompt("Quantos anos você tem?")
+idade>=18 ? console.log ("pode tirar carteira de motorista!") : console.log("Ainda não pode tirar carteira de motorista!")
+
+
+
