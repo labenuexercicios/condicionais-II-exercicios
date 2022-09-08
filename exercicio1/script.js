9089fd8858f3e86ae32037699e3329afb74962ea
xVieiraLucas <lucas.brazilianpilo@gmail.com>
2022-09-08 14:08:14 -0300

    exericios-pré-correção

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,48 @@
-const num = //seu numero aqui
+// # Exercício 1
 
+// Crie um código que receba um número por **prompt** e verifique se um número é divisível por 2 **ou** por 3
+    
+// Faça isso:
+    
+// 1. Utilizando ifs aninhados
+// ```jsx
+//     if(expressao){
+//         if(expressao){
+
+//         }
+//     }
+// ```
+// 2. Utilizando um operador lógico para unir duas operações relacionais
+// ```jsx
+//     if(expressao && expressao){ //&& para E
+//     //expressao || expressao para OU     
+    
+
+const numero = Number(prompt('Insira um número'))
+
+if(numero % 2 === 0){
+    console.log (numero + ' é divisivel por 2') 
+    if(numero % 3 === 0){
+        console.log (numero + ' é divisivel por 3')
+    } 
+} else if(numero % 3 === 0){
+        console.log (numero + ' é divisivel por 3')
+} else {
+    console.log('Número primo')
+}
+
+
+if(numero % 2 === 0){
+    console.log (numero + ' é divisivel por 2') 
+} else if(numero % 3 === 0){
+    console.log (numero + ' é divisivel por 3')
+} else {
+    console.log('Número primo')
+}
+
+
+
+if(numero % 2 === 0 || numero % 3 === 0){
+    console.log('Este número é divisivel por 2 OU 3')
+} else {
+    console.log('Este número é primo') }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,22 @@
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+
+const nome = prompt('Insira um nome')
+const idade = Number(prompt('Insira um idade'))
+
+nome === 'José' ? console.log('Oi, Zé!') : console.log('Olá, ' +nome)
+
+idade >= 18 ? console.log('Pode tirar carteira de motorista!') : console.log('Ainda não pode tirar carteira de motorista')
+
+
