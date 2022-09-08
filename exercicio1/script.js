68092ea6b4dca59b22569b6de694983ea97ddb6d
lucasrodriguesg <lucas.rodrigues.pvd@gmail.com>
2022-09-08 20:58:58 -0300

    Exercicios do dia

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,10 @@
-const num = //seu numero aqui
+const num = +prompt("Digite um numero")
 
+if (num % 2 === 0){
+    console.log("É divisivel por 2")
+}
+if (num % 3 === 0){
+    console.log("É divisivel por 3")
+}
+
+console.log (num % 2 === 0 || num % 3 === 0 ? "É divisivel por 2 ou por 3" : "Não é divisivel por 2 ou 3")
