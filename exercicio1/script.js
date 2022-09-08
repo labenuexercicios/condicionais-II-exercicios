38a122ef0be61fb6c441d555414fea5efd4b166e
rafaela-soares-a <raffasoares95@gmail.com>
2022-09-08 17:31:52 -0300

    Atividade concluida

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,20 @@
-const num = //seu numero aqui
+const num = +prompt("Insira um número")
 
+if (num % 2 === 0) { 
+    console.log("Esse número é disivível por 2")
+    if (num % 3 === 0)
+        console.log("Esse número é divisível por 2 e por 3")
+} else {
+    console.log("Esse número não é divisível por 2")
+    if (num % 3 === 0) {
+        console.log("Esse número é divisivel por 3")
+    } else {
+        console.log ("Esse número não é divisível")
+    }
+}
+
+// if (num %2 === 0 || num % 3 ===0 ) {
+//     console.log("O número é divisivel por 2 ou por 3")
+//     } else {
+//         console.log ("O número não é divisível por 2 nem por 3")
+//     }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,19 @@
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+
+let nome = prompt ("Digite seu nome")
+console.log (nome === "jose".toLocaleLowerCase() ? 'Olá Zé' : 'Você não é o Zé')
+
+let idade = prompt ("Quantos anos você tem?")
+console.log (idade >=18 ? "Você pode tirar carteira de motorista" : "Ainda não pode tirar sua CNH")
