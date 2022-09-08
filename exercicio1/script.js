d4b4c9babf09b0b9e689f45df3db8eb5ba463788
igorazuos <azuosigor@gmail.com>
2022-09-08 11:01:23 -0300

    atualização exercicio a013

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,12 @@
-const num = //seu numero aqui
+const num = Number(prompt("diga um numero"))
+if(num % 2 === 0){
+    console.log("esse numero é divisivel por 2")
+    if(num % 3 === 0){
+        console.log("esse numero é divisivel por 3")
+    }else{
+        console.log("esse numero não é divisivel por 3")
+    }
+}else{
+    console.log("esse numero não é divisivel por 2")
+}
 
