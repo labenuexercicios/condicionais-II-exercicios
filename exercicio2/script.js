48b82a1759ceb61079e8692d5a0b9b3dd7df1391
JP-Becker <jpbecker20@gmail.com>
2022-09-08 16:14:09 -0300

    AmmalB-a013-JoaoBecker

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,27 @@
-let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
+//Recrie o exercício 3 da aula anterior (A012) de condicionais I, utilizando switch-case. No arquivo `script,js` existe uma versão do código já escrita, mas você pode colar o exercício que você fez na última prática!
 
-if(nacionalidade === "brasileira"){
-    console.log("a pessoa é do Brasil!");
-} else if(nacionalidade === "argentina"){
-    console.log("a pessoa é da Argentina!");
-} else if(nacionalidade === "uruguaia"){
-    console.log("a pessoa é do Uruguai!");
-} else if(nacionalidade === "chilena"){
-    console.log("a pessoa é do Chile!");
-} else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!");
-} else{
-    console.log("nacionalidade não encontrada")
+
+const nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
+
+switch(nacionalidade){
+	case "brasileira":        
+        alert('Que pena, você é do: ' + 'Brasil :/')
+		break;
+    case "argentina":        
+        alert('Você é da: ' + 'Argentina!')
+        break;
+    case "uruguaia":        
+        alert('Você é do: ' + 'Uruguai!')
+        break;
+    case "chilena":        
+        alert('Você é do: ' + 'Chile!')
+        break;
+    case "colombiana":        
+        alert('Você é da: ' + 'Colômbia!')
+        break;
+    default:
+        alert('Você é um Alien!!!')
 }
+
+
+
