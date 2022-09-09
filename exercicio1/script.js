a090e92cf0465cc6d880f12f15a0f43315f55429
Rafaella Estecio <rafaellaestecio@gmail.com>
2022-09-08 21:39:23 -0300

    resolução exercicio condicional

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,25 @@
-const num = //seu numero aqui
+
+
+// # Exercício 1
+// Crie um código que receba um número por **prompt** e 
+// verifique se um número é divisível por 2 **ou** por 3:
+    
+const num = Number (prompt("Digite um número"));
+
+//1. Utilizando ifs aninhados
+//2. Utilizando um operador lógico para unir duas operações relacionais
+if (num % 2 === 0 || num % 3 === 0){
+    
+    if (num % 2 === 0 && num % 3 === 0){
+        console.log("É divisivel por 2 e por 3");
+    }else if (num % 2 === 0){
+        console.log("É divisivel por 2");
+    }else {
+        console.log("É divisivel por 3");
+    }
+
+}else {
+    console.log("Não é divisivel nem por 2 ou 3");
+}
+
 
