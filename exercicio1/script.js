230c80b39bd0447b9fa234891174386945412bb8
dorotrodrigues <dorotbarbosa57@gmail.com>
2022-09-08 23:43:56 -0300

    Atividades 013 finalizadas

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,44 @@
-const num = //seu numero aqui
+// # Exercício 1
 
+// Crie um código que receba um número por **prompt** e verifique se um número é divisível por 2 **ou** por 3
+// Faça isso:
+    
+// 1. Utilizando ifs aninhados
+//  ```jsx
+//      if(expressao){
+//          if(expressao){
+
+//         }
+//      }
+//  ```
+    
+
+const num = Number(prompt("Digite um número:"))
+
+if(num % 2 === 0){
+    if(num % 3 === 0){
+        console.log("Número divisível por 2 e por 3.")
+    }
+    else {
+        console.log("Número é divisível por 2 e não por 3.")
+    }
+}
+else{
+    console.log("Número não é divisível por 2.")
+}
+
+// 2. Utilizando um operador lógico para unir duas operações relacionais
+// ```jsx
+//     if(expressao && expressao){ //&& para E
+//     //expressao || expressao para OU     
+    
+//     }
+// ```
+
+if(num % 2 === 0 && num % 3 === 0){
+    console.log("Número divisível por 2 e por 3.")
+} else if(num % 2 === 0 || num % 2 === 0){
+    console.log("Número é divisível por 2 ou por 3.")
+} else { 
+    console.log("Número não é divisível por 2 nem por 3.")
+}
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,35 @@
+// # Exercício 3
+
+// Reescreva os códigos do arquivo script.js utilizando o if ternário.
+
+// **Relembrando o if ternário**
+// ```jsx
+// condição ? expr1 : expr2
+// ```
+
+// Onde :
+
+// `condição` é uma expressão que é avaliada como `true` ou `false`.
+
+// `expr1`, `expr2` são xpressões com valores de qualquer tipo.
+
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+
+const nome = prompt("Qual o seu nome?")
+const idade = prompt("Qual a sua idade?")
+
+nome === "José" ? console.log("Oi, Zé!") : console.log("Olá, "+nome);
+
+idade >= 18 ? console.log("pode tirar carteira de motorista!") : console.log("Ainda não pode tirar carteira de motorista");
