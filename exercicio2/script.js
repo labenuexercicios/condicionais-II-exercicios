da740a0dfb0b85ee0e3f0e60dccbfb43bd351c1b
Andersom <16ammp@gmail.com>
2022-10-06 20:46:55 -0300

    exercícios, condicionais II, aula 13, labenu

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,43 @@
+// Recrie o exercício 3 da aula anterior (A012) de condicionais I, utilizando switch-case. No arquivo script,js existe uma versão do código 
+// já escrita, mas você pode colar o exercício que você fez na última prática!
+
 let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
 
-if(nacionalidade === "brasileira"){
-    console.log("a pessoa é do Brasil!");
-} else if(nacionalidade === "argentina"){
-    console.log("a pessoa é da Argentina!");
-} else if(nacionalidade === "uruguaia"){
-    console.log("a pessoa é do Uruguai!");
-} else if(nacionalidade === "chilena"){
-    console.log("a pessoa é do Chile!");
-} else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!");
-} else{
-    console.log("nacionalidade não encontrada")
+// if(nacionalidade === "brasileira"){
+//     console.log("a pessoa é do Brasil!");
+// } else if(nacionalidade === "argentina"){
+//     console.log("a pessoa é da Argentina!");
+// } else if(nacionalidade === "uruguaia"){
+//     console.log("a pessoa é do Uruguai!");
+// } else if(nacionalidade === "chilena"){
+//     console.log("a pessoa é do Chile!");
+// } else if(nacionalidade === "colombiana"){
+//     console.log("a pessoa é da Colômbia!");
+// } else{
+//     console.log("nacionalidade não encontrada")
+// }
+
+
+switch(nacionalidade){
+    case "brasileira":
+        console.log("a pessoa é do Brasil!");
+        break;
+
+    case "argentina":
+        console.log("a pessoa é da Argentina!");
+        break;
+
+    case "uruguaia":
+        console.log("a pessoa é do Uruguai!");
+        break;
+
+    case "chilena":
+        console.log("a pessoa é do Chile!");
+        break;
+
+    case "colombiana":
+        console.log("a pessoa é da Colômbia!");
+        break;
+
+    default: console.log("nacionalidade não encontrada")
 }
