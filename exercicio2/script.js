c063dca0522fd2a20bc887b2f0419c9d36aa0fdf
Lucas Breia <lucas@MacBook-Air-de-Lucas.local>
2022-11-14 23:22:39 -0300

    Exercícios de Treinamento

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,17 +1,23 @@
-let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
+//CONDICIONAIS II - EXERCÍCIO 2
+//SWITCH CASE
+let nacionalidade = prompt('Digite sua nacionalidade:').toLowerCase();
 
-if(nacionalidade === "brasileira"){
-    console.log("a pessoa é do Brasil!");
-} else if(nacionalidade === "argentina"){
-    console.log("a pessoa é da Argentina!");
-} else if(nacionalidade === "uruguaia"){
-    console.log("a pessoa é do Uruguai!");
-} else if(nacionalidade === "chilena"){
-    console.log("a pessoa é do Chile!");
-} else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!");
-} else{
-    console.log("nacionalidade não encontrada")
+switch(nacionalidade){
+    case 'brasileira': 
+        console.log('Você é do Brasil!');
+        break
+    case 'argentina':
+        console.log('Você é da Argentina!');
+        break
+    case 'uruguaia':
+        console.log('Você é do Uruguai!');
+        break
+    case 'chilena':
+        console.log('Você é do Chile!');
+        break
+    case 'colombiana':
+        console.log('Você é da Colômbia!');
+        break
+    default:
+        console.log('Nacionalidade não encontrada.')
 }
-
-console.log("teste 2")
