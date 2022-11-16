092611e69b1e5dd257838de20c4f3dfb8c6b53bb
gomesvini <pain-nici2011@live.com>
2022-11-16 00:04:01 -0300

    Exercicios de condicionais II finalizados

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,16 +1,25 @@
 //código a ser reescrito
+const nome = prompt(`Escreva seu nome:`)
+const idade = Number(prompt(`Escreva sua idade:`))
 
-if(nome === "José"){
+/* if(nome === "José"){
 	console.log("Oi, Zé!");
 } else {
 	console.log("Olá, "+nome);
-}
+} */
 
-if(idade >= 18){
+nome === "José" ? 
+	console.log(`Oi, Zé!`) : 
+	console.log(`Ola, ${nome}`); 
+
+
+	/* if(idade >= 18){
 	console.log("pode tirar carteira de motorista!");
 } else {
 	console.log("Ainda não pode tirar carteira de motorista");
-}
+} */
+idade >= 18 ?
+	console.log(`Pode tirar carteira de motorista!`) :
+	console.log(`Ainda não pode tirar carteira de motorista.`);
 
 
-console.log("teste 3")
