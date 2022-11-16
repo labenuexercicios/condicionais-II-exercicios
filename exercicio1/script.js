6df8f1a436c9b128063629a3d104cbc84c46a539
socrampontes <114445301+socrampontes@users.noreply.github.com>
2022-11-16 20:28:39 -0300

    exercicio de condicionaisII

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,14 @@
-console.log("teste 1")
+const numero = prompt('digite um numero')-0
 
+if(numero%2==0){
+    console.log('numero divisivel por 2 ')
+    if(numero%3==0){
+        console.log('numero divisivel por 3')}
+}else{
+    console.log('o numero não é divisivel por 2 nem por 3')
+}
+if(numero%2==0||numero%3==0){
+    console.log('numero divisivel por 2 ')
+}else{
+    console.log('o numero não é divisivel por 2 nem por 3')
+}
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,5 +1,21 @@
 //código a ser reescrito
+/* # Exercício 3
 
+Reescreva os códigos do arquivo script.js utilizando o if ternário.
+
+**Relembrando o if ternário**
+```jsx
+condição ? expr1 : expr2
+```
+
+Onde :
+
+`condição` é uma expressão que é avaliada como `true` ou `false`.
+
+`expr1`, `expr2` são xpressões com valores de qualquer tipo. */
+
+let nome = prompt('digite seu nome').toLowerCase()
+let idade = prompt('qual sua idade?')+0
 if(nome === "José"){
 	console.log("Oi, Zé!");
 } else {
@@ -12,5 +28,5 @@ if(idade >= 18){
 	console.log("Ainda não pode tirar carteira de motorista");
 }
 
-
-console.log("teste 3")
+nome=="josé"? console.log("Oi, Zé!"):console.log("Olá, "+nome)
+idade>=18? console.log("pode tirar carteira de motorista!"):console.log("Ainda não pode tirar carteira de motorista")
