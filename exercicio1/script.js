882974bfbfe1b3e189be73bc0be3eb2847d4aa2e
Kenzie <silvadesouzaanalice92@gmail.com>
2022-12-01 15:52:09 -0300

    corrigido

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -3,16 +3,38 @@
 let numero = parseInt(prompt("Digite um número"));
 
 if (numero % 2 === 0){
-
+    console.log("o número é divisível por 2");
     if (numero % 3 === 0){
         console.log("o número é divisível por 3");
+    }else{
+        console.log("o número não é divisível por 3 ");
     }
-    else{
-        console.log("o número não é divisível por 3");
+    if (numero % 2 === 0 && numero % 3 === 0){
+        console.log("o número é divisível por 2 e por 3");
     } 
-}else{
+    else{
+        console.log("o número não é divisível por 2 e por 3");
+    }
+  } else{
     console.log("o número não é divisível por 2");
 }
+
+/*
+const numero = parseInt(prompt("digite um número")) 
+
+if(numero % 2 === 0){
+    if(numero % 3 === 0){
+    console.log(`O número ${numero} é divisível por 2 e por 3`)
+  }else {
+    console.log(`O número ${numero} é divisível por 2`)}
+}
+else if(numero % 3 === 0){
+    console.log(`O número ${numero} é divisível por 3`)
+}else{
+    console.log(`O número ${numero} não é divisível por 2 e por 3`)}
+*/
+
+
 
 //////////////////////////////////////////////////////////////////
 /*
