8e4437b053bbb6dd870181f24068fc9382c399c2
Rottschaefer <rottschaefer54@gmail.com>
2022-12-01 10:46:23 -0300

    Exercício Condicionais II feito

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,29 @@
 console.log("teste 1")
 
+let num = prompt("Escreva um número")
+let i = 0
+
+num = Number(num)
+
+if(num%2==0){
+    if(num%3==0){
+        console.log("É divisível por 2 e por 3")
+        i = 1
+    }
+    else if(i==0){
+    console.log("É divisível por 2")
+
+}}
+
+if(i==0){
+    if(num%3==0){
+    console.log("É divisível por 3")
+}}
+
+
+
+
+if(num%2==0 || num%3==0){
+    console.log("É divisível por 2 ou por 3")
+
+}
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,16 +1,24 @@
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
 
 
 console.log("teste 3")
+
+let nome = "Edu"
+let idade = 22
+
+console.log(nome === "José"? "Oi, Zé!" : "Olá, "+nome)
+
+
+console.log(idade >= 18? "Pode tirar carteira de motorista!" : "Ainda não pode tirar carteira de motorista")
