6a36a366b87d2662ddfffd617b5a0a0a06de0854
eesfernandes <eesf@hotmail.com>
2023-03-02 11:23:58 -0300

    condicionais-II-exercicios

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,16 +1,23 @@
 //códigos a serem reescritos
 
-// a)
-if(nome === "José"){
-	console.log("Oi, Zé!")
-} else {
-	console.log("Olá, " + nome)
-}
+let nome = prompt(`Digite um nome: `).toLowerCase();
 
+let idade = parseInt(prompt(`Digite a idade: `));
+
+// a)
+// if(nome === "José"){
+// 	console.log("Oi, Zé!")
+// } else {
+// 	console.log("Olá, " + nome)
+// }
+
+nome === 'Jose' ? console.log("Oi, Zé!") : console.log("Olá, " + nome);
 
 // b)
-if(idade >= 18){
-	console.log("Pode tirar carteira de motorista!")
-} else {
-	console.log("Ainda não pode tirar carteira de motorista!")
-}
+// if(idade >= 18){
+// 	console.log("Pode tirar carteira de motorista!")
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista!")
+// }
+
+idade >= 18 ? console.log("Pode tirar carteira de motorista!") : console.log("Ainda não pode tirar carteira de motorista!")
