abca0390efcdf51c10e7afc20a4f697fa52de102
LivanDoVale <livanjr4@gmail.com>
2023-03-06 16:00:20 -0300

    Tarefas feitas

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,6 +1,6 @@
 //código a ser reescrito
 
-if(nome === "José"){
+/*if(nome === "José"){
 	console.log("Oi, Zé!");
 } else {
 	console.log("Olá, "+nome);
@@ -12,5 +12,13 @@ if(idade >= 18){
 	console.log("Ainda não pode tirar carteira de motorista");
 }
 
-
 console.log("teste 3")
+*/
+
+let nome = prompt("Digite seu nome").toLowerCase()
+
+nome === "jose" ? console.log("Oi,Zé!") : console.log("Olá,", nome)
+
+let idade = Number(prompt("Digite sua idade"))
+
+idade >= 18 ? console.log("Pode tirar carteira de motorista") : console.log("Não pode tirar carteira de motorista")
