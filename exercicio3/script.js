0c35f8fec59dd39ea36bdb3cd87db6e89f1f5d5f
raissanfg <99144281+raissanfg@users.noreply.github.com>
2023-09-19 12:47:24 -0500

    exercicio-3-feito

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,5 +1,5 @@
 //códigos a serem reescritos
-
+let nome = prompt("Digite seu nome")
 // a)
 if(nome === "José"){
 	console.log("Oi, Zé!")
@@ -7,6 +7,9 @@ if(nome === "José"){
 	console.log("Olá, " + nome)
 }
 
+nome === "José"? console.log("Oi, Zé!") : console.log("Olá, " + nome)
+
+let idade = prompt("Digite sua idade")
 
 // b)
 if(idade >= 18){
@@ -14,3 +17,5 @@ if(idade >= 18){
 } else {
 	console.log("Ainda não pode tirar carteira de motorista!")
 }
+
+idade >= 18 ? console.log("Pode tirar carteira de motorista!") : console.log("Ainda não pode tirar carteira de motorista!")
